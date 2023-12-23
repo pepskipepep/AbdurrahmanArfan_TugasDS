b7168c117a6832acb1657fb51e19b09bfa39c7ab
Abdurrahman <arfan407@gmail.com>
2023-12-23 19:09:36 +0700

    delete comment

--- src/test/java/org/example/SeleniumTest.java
+++ src/test/java/org/example/SeleniumTest.java
@@ -16,42 +16,35 @@ public class SeleniumTest {
         WebDriver driver = new EdgeDriver();
         driver.manage().window().maximize();
         
-                // 1. Buka Yopmail.com
         driver.get("http://www.yopmail.com/");
 
-        // 2. Search email "automationtest"
         WebElement searchBox = driver.findElement(By.id("login"));
         searchBox.sendKeys("automationtest");
         WebElement checkInboxButton = driver.findElement(By.cssSelector("input.sbut"));
         checkInboxButton.click();
 
-        // Tunggu sebentar untuk memastikan halaman sudah ter-load
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // 3. Switch ke iframe dan extract content di email paling pertama
         driver.switchTo().frame("ifinbox");
 
         // Klik pada email pertama
         WebElement firstEmail = driver.findElement(By.cssSelector("div.mls.mb0 > a"));
         firstEmail.click();
 
-        // Tunggu sebentar untuk memastikan email terbuka
         try {
             Thread.sleep(2000);
         } catch (InterruptedException e) {
             e.printStackTrace();
         }
 
-        // Extract content email
         WebElement emailContent = driver.findElement(By.id("mailmillieu"));
         String content = emailContent.getText();
         System.out.println("Email Content:\n" + content);
 
-        // Tutup browser
         driver.quit();
     }
 
