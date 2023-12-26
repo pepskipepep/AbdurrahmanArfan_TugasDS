d7f2ec10bcd14c96cdf1031b726d74fb1cec5c95
Abdurrahman <arfan407@gmail.com>
2023-12-26 10:43:41 +0700

    [FIX] clear comment

--- src/test/java/org/example/SeleniumTest.java
+++ src/test/java/org/example/SeleniumTest.java
@@ -20,29 +20,14 @@ public class SeleniumTest {
         // Buka makemytrip.com
         driver.get("https://www.makemytrip.com/");
 
-        // Cari elemen kotak pencarian dan masukkan kata kunci
-        // WebElement searchBox = driver.findElement(By.id("fromCity"));
-        // searchBox.sendKeys("New York");
-
-        // Tunggu beberapa saat agar dropdown muncul dan pilih kota dari dropdown
-        // (Perhatikan bahwa ini hanya langkah sederhana, dan Anda mungkin perlu menyesuaikannya dengan perilaku aktual situs web)
-        // ...
-
-        // Cari tombol untuk mencari dan klik
-        // WebElement searchButton = driver.findElement(By.xpath("//button[@class='fsw_search__btn']"));
         WebElement searchButton = driver.findElement(By.cssSelector("a.widgetSearchBtn"));
         searchButton.click();
 
-        // Tunggu beberapa saat agar hasil pencarian muncul dan cari elemen harga teratas
-        // WebElement topPriceElement = driver.findElement(By.xpath("//span[@class='actual-price']"));
         try {
-            // Menunggu hingga pop-up muncul
             Thread.sleep(10000);
 
-            // Mendapatkan instance dari alert
             WebElement closePopUp = driver.findElement(By.cssSelector("span.bgProperties.overlayCrossIcon.icon20"));
 
-            // Menutup pop-up dengan mengonfirmasi
             closePopUp.click();
 
         } catch (Exception e) {
@@ -57,15 +42,10 @@ public class SeleniumTest {
 
 
         WebElement topPriceElement = driver.findElement(By.cssSelector("div.blackText.fontSize18.blackFont.white-space-no-wrap.clusterViewPrice']"));
-        // WebElement topPriceElement = driver.findElement(By.xpath("//div[@class='clusterViewPrice']"));
-        // WebElement topPriceElement = driver.findElement(By.xpath("//div[@class='clusterViewPrice']"));
-        // Ambil nilai teks dari elemen harga teratas
         String topPrice = topPriceElement.getText();
 
-        // Cetak harga teratas
         System.out.println("Top Price: " + topPrice);
 
-        // Tutup browser
         driver.quit();
     }
 
